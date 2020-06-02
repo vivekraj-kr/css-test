ad9b421793aad408fc7c3374ce438eae95e2e027
vivekraj k r <vivekrajkr@vivekrajs-MacBook-Air.local>
2020-06-03 01:55:47 +0530

    Update the port

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require('express');
-var server = express();
-server.use('/', express.static(__dirname + '/'));
-server.listen(8080);
+var app = express();
+app.use('/', express.static(__dirname + '/'));
+var server = app.listen(process.env.PORT || 8081, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
